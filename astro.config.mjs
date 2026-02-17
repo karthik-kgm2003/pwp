5dbc6877c18701a301938af89741c60e6d96727e
karthik-kgm2003 <karthikgmenon.kgm@gmail.com>
2026-02-17 02:18:11 +0100

    git commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Replace with your actual GitHub username
+  site: 'https://karthikmenon.github.io', 
+  // If your repo is karthikmenon.github.io, use '/'. 
+  // If your repo is named 'portfolio', use '/portfolio/'
+  base: '/pwp/', 
+  integrations: [tailwind()],
+});
